16529f7d9fac96223fa0d0aac740d9436b79ad58
Henry Sztanski <henry@sztanski.com>
2019-03-27 19:19:08 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 class MusicLibraryController
-  attr_accessor :path, :importer, :files
+  attr_accessor :path, :importer
 
   def initialize(path = "./db/mp3s")
     @path = path
@@ -29,7 +29,11 @@ class MusicLibraryController
     files = self.importer.files.collect{|file_name| file_name.gsub(".mp3", "")}
     files.sort_by!{|file_name| file_name.split(" - ")[1]}
     files.each.with_index(1) {|file, index| puts "#{index}. #{file}"}
-    @files = files
+  end
+
+  def ordered_files #same as list_songs but does not use puts only for spec purposes
+    files = self.importer.files.collect{|file_name| file_name.gsub(".mp3", "")}
+    files.sort_by!{|file_name| file_name.split(" - ")[1]}
   end
 
   def list_artists
@@ -63,12 +67,11 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     song_number = gets.chomp.to_i
-    song_names = self.list_songs
+    song_names = self.ordered_files #had to create #ordered_files because #list_songs has puts statements which conflict with the test
     if song_number > 0 && song_number <= song_names.size
-      song_name = song_names.split(" - ")[1]
-      artist_name = song_names.split(" - ")[0]
-      "Playing #{song_name} by #{artist_name}"
-      binding.pry
+      song_name = song_names[song_number - 1].split(" - ")[1]
+      artist_name = song_names[song_number - 1].split(" - ")[0]
+      puts "Playing #{song_name} by #{artist_name}"
     end
   end
 end
